72703ba3139daf64cf46a52fc3e99b85b06b6933
“einsy-dev” <116911954+einsy-dev@users.noreply.github.com>
2023-05-04 21:35:23 +0600

    start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ShopItem from './ShopItem';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShopItem
+      brand="Tiger of Sweden"
+      title='Leonard coat'
+      description='Minimalistic coat in cotton-blend'
+      descriptionFull='Mens minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.'
+      price='399'
+      currency='£'
+    />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
